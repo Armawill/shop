43e4d599e72aeabf9c5bc335833aea4b45957123
Armawill <armawill@gmail.com>
2022-06-10 15:42:47 +0300

    Update order_item.dart

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -36,32 +36,34 @@ class _OrderItemState extends State<OrderItem> {
               },
             ),
           ),
-          if (_expanded)
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: min(widget.order.products.length * 20.0 + 10, 100),
-              child: ListView(
-                children: widget.order.products
-                    .map((prod) => Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              prod.title,
+          AnimatedContainer(
+            duration: Duration(milliseconds: 300),
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+            height: _expanded
+                ? min(widget.order.products.length * 20.0 + 10, 100)
+                : 0,
+            child: ListView(
+              children: widget.order.products
+                  .map((prod) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            prod.title,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text('${prod.quantity}x \$${prod.price}',
                               style: TextStyle(
                                 fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text('${prod.quantity}x \$${prod.price}',
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.grey,
-                                )),
-                          ],
-                        ))
-                    .toList(),
-              ),
-            )
+                                color: Colors.grey,
+                              )),
+                        ],
+                      ))
+                  .toList(),
+            ),
+          )
         ],
       ),
     );
